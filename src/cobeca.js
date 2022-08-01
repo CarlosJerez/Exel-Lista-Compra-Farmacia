09b7f5731cf379eaf932b272f2300d735257765c
Carlo Jerez <61286650+CarlosJerez@users.noreply.github.com>
2022-08-01 17:53:38 -0400

    Mejorando preferencias en lista exel campos nuevos

--- src/cobeca.js
+++ src/cobeca.js
@@ -6,6 +6,11 @@ const cobecaList = cobeca.map(item => {
         "cod_barra": item.cod_barra,
         "desc_articulo": item.desc_articulo,
         "proveedor": item.proveedor.descripcion,
+        "porcentaje": item.porcentaje_cobeca,
+        "precio": item.monto_final,
+        "diasCredito": item.dias,
+        "componenteBase": item.componenteBase.descripcion,
+        'existencia': item.existencia,
     }
     return itemR
 })
--- src/index.js
+++ src/index.js
@@ -11,14 +11,10 @@ function leerExcel(ruta) {
   return dataExcel;
 }
 
-const farmaciaList = leerExcel(
-  path.join(__dirname, "..", "exelFarmacia", "exelFarmacia.xlsx")
-);
+const farmaciaList = leerExcel(path.join(__dirname, "..", "exelFarmacia", "exelFarmacia.xlsx"));
 
 const farmaciaListFinal = farmaciaList.map((item) => {
-  const cobecaItem = cobecaList.filter(
-    (cobecaItem) => cobecaItem.cod_barra == item.Codigo
-  );
+  const cobecaItem = cobecaList.filter((cobecaItem) => cobecaItem.cod_barra == item.Codigo);
   const cobecaItemFinal = cobecaItem.map((item2) => {
     return item2;
   });
@@ -39,20 +35,30 @@ ws.cell(1, 3).string("Codigo");
 ws.column(3).setWidth(20);
 ws.cell(1, 4).string("Descripcion");
 ws.column(4).setWidth(50);
-ws.cell(1, 5).string("Laboratorio");
-ws.column(5).setWidth(50);
-ws.cell(1, 6).string("Cantidad");
-ws.cell(1, 7).string("Existencia");
+ws.cell(1, 5).string("Componente");
+ws.column(5).setWidth(25);
+ws.cell(1, 6).string("Laboratorio");
+ws.column(6).setWidth(30);
+ws.cell(1, 7).string("Cantidad");
+ws.cell(1, 8).string("Existencia");
+ws.cell(1, 9).string("Descuento");
+ws.cell(1, 10).string("Precio");
+ws.cell(1, 11).string("Existencia");
+ws.cell(1, 12).string("Dias. Cr");
 
 farmaciaListFinal.forEach(function (item, index) {
-  if (item.data != undefined)
-    ws.cell(index + 2, 1).number(item.data.cod_articulo);
-  ws.cell(index + 2, 2).number(1);
+  if (item.data != undefined) ws.cell(index + 2, 1).number(item.data.cod_articulo);
+  //ws.cell(index + 2, 2).number(0);
   ws.cell(index + 2, 3).string(`${item.Codigo}`);
   ws.cell(index + 2, 4).string(`${item.Descripcion}`);
-  if (item.data != undefined) ws.cell(index + 2, 5).string(item.data.proveedor);
-  ws.cell(index + 2, 6).number(item.Cantidad);
-  ws.cell(index + 2, 7).number(item.Existencia);
+  if (item.data != undefined) ws.cell(index + 2, 5).string(item.data.componenteBase);
+  if (item.data != undefined) ws.cell(index + 2, 6).string(item.data.proveedor);
+  ws.cell(index + 2, 7).number(item.Cantidad);
+  ws.cell(index + 2, 8).number(item.Existencia);
+  if (item.data != undefined) ws.cell(index + 2, 9).number(item.data.porcentaje);
+  if (item.data != undefined) ws.cell(index + 2, 10).number(item.data.precio);
+  if (item.data != undefined) ws.cell(index + 2, 11).number(item.data.existencia);
+  if (item.data != undefined) ws.cell(index + 2, 12).number(item.data.diasCredito);
 });
 
 const pathExel = path.join(__dirname, "..", "exelCobeca", "exelCobeca.xlsx");
@@ -61,6 +67,6 @@ wb.write(pathExel, function (err, stats) {
   if (err) {
     console.error(err);
   } else {
-    console.log("archivo creado");
+    console.log("archivo creado" + " " + pathExel);
   }
 });
